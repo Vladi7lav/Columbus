f709fb622a76e0c6d27e002a4cf4169189f884a0
Vladislav Tsirulev <Vladislav.Tsirulev@columbusglobal.com>
2019-04-22 18:40:53 +0300

    Lab3
    lab3

---
+++ Lab3V2/Lab3V2/CrmServiceWrapper.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3V2
+{
+    class CrmServiceWrapper
+    {
+        private OrganizationServiceProxy organizationServiceProxy
+        {
+            get;
+            set;
+        }
+        //private OrganizationServiceContext organizationServiceContext
+        //{
+        //    get;
+        //    set;
+        //}
+
+        public CrmServiceWrapper(Uri serviceUri, ClientCredentials credentials)
+        {
+            this.organizationServiceProxy = new OrganizationServiceProxy(serviceUri, null, credentials, null);
+            //this.organizationServiceContext = new OrganizationServiceContext(this.organizationServiceProxy);
+
+        }
+
+        public EntityCollection QueryExpressionExample()
+        {
+            try
+            {
+                Entity lead = new Entity("contact");
+
+                QueryExpression qContact = new QueryExpression("contact");
+                ColumnSet cset = new ColumnSet();
+                FilterExpression filterJobMan = new FilterExpression();
+                ConditionExpression condExp = new ConditionExpression();
+                cset.AddColumns("fullname", "parentcustomerid");
+                condExp.AttributeName = "jobtitle";
+                condExp.Operator = ConditionOperator.Equal;
+                condExp.Values.Add("Менеджер по закупкам");
+                filterJobMan.AddCondition(condExp);
+                qContact.ColumnSet = cset;
+                qContact.Criteria.AddFilter(filterJobMan);
+                EntityCollection contacts = this.organizationServiceProxy.RetrieveMultiple(qContact);
+
+                return contacts;
+            }
+            catch
+            {
+                Console.WriteLine();
+                //organizationServiceContext.Dispose();
+                organizationServiceProxy.Dispose();
+                return null;
+            }
+        }
+    }
+}
---
+++ Lab3V2/Lab3V2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var credentials = new ClientCredentials
+            {
+                Windows = { ClientCredential = new NetworkCredential("Administrator", "Pass@word99") }
+            };
+
+            Uri serviceUri = new Uri("http://crm-train.columbus.ru:5555/CRM2016/XRMServices/2011/Organization.svc");
+            EntityCollection contacts = new CrmServiceWrapper(serviceUri, credentials).QueryExpressionExample();
+            foreach (var c in contacts.Entities)
+            {
+                Console.WriteLine(c.Attributes["fullname"].ToString());
+            }
+            Console.Read();           
+        }
+    }
+}
